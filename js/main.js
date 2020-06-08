00fec38cfb22b7a0ac49e3cab501e0e64b75052e
Litvinov Yuriy <u.litvinov@elewise.com>
2020-06-08 12:10:36 +0400

    init(main): slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+
+
+$(document).ready(function() {
     // настройка плагина
     $('.main').onepage_scroll({
         sectionContainer: 'section',
@@ -17,5 +19,22 @@ $(document).ready(function(){
             $(el).removeClass('active');
         });
         $(this).addClass('active');
-    })
+       
+        if($(this).hasClass('page2__variant')) {
+            showNextMaterial(this, materials, '.page2');
+        }
+    });
 });
+
+function showNextMaterial(selector, variants, wrapSelector) {
+    var num = $(selector).data('num');
+    var currentData = variants[num];
+    var templateText = `<h3>${ currentData.title }</h3>`;
+    currentData.text.forEach(p => templateText += `<p>${p}</p>`);
+    $(wrapSelector).css('background-image', `url(./images/${ currentData.img })`);
+    $(`${ wrapSelector }__text`).addClass('hide');
+    setTimeout(() => {
+        $(`${ wrapSelector }__text`).removeClass('hide');
+        $(`${ wrapSelector }__text`).html(templateText);
+    }, 300);
+}
