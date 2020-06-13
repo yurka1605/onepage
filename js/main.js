e149362931211e6ae01da2b0355de7b380a064c3
Litvinov Yuriy <u.litvinov@elewise.com>
2020-06-13 12:41:26 +0400

    init(main): mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 
 
 $(document).ready(function() {
+    if ($(window).width() < 769) {
+        $('.page3').remove();
+    }
+    $(window).resize(function () {
+        location.reload();
+    });
     // настройка плагина
     $('.main').onepage_scroll({
         sectionContainer: 'section',
-        responsiveFallback: 600,
+        responsiveFallback: 320,
         pagination: true,
         loop: true,
         keyboard: true,
@@ -28,9 +34,7 @@ $(document).ready(function() {
        
         if($(this).hasClass('materials1')) {
             showNextMaterial(this, materials1, '.page2');
-        }
-       
-        if($(this).hasClass('materials2')) {
+        } else {
             showNextMaterial(this, materials2, '.page5');
         }
     });
@@ -54,6 +58,48 @@ $(document).ready(function() {
     $('button.callme-btn').on('click', function (e) {
         e.preventDefault();
         $('body').addClass('popup-on');
+    });
+
+    $('.page2, .page5').swipe( {
+        swipeStatus: function(event, phase, direction, distance, duration, fingerCount, fingerData, currentDirection) {
+            if (phase=="end") {
+                //сработает через 20 пикселей то число которое выбрали в threshold
+                if (direction == 'left') {
+                    //сработает при движении влево
+                    var next = '';
+                    if ($(this).hasClass('page2')) {
+                        var num = $('.materials1.active').data('num');
+                        next = num !== 7 ? $('.materials1.active').next() : $('.materials1:first-child');
+                        $('.materials1.active').removeClass('active');
+                        showNextMaterial(next, materials1, '.page2');
+                    } else {
+                        var num = $('.materials2.active').data('num');
+                        next = num !== 7 ? $('.materials2.active').next() : $('.materials2:first-child');
+                        $('.materials2.active').removeClass('active');
+                        showNextMaterial(next, materials2, '.page5');
+                    }
+                    $(next).addClass('active');
+                }
+                if (direction == 'right') {
+                    //сработает при движении вправо
+                    var prev = '';
+                    if ($(this).hasClass('page2')) {
+                        var num = $('.materials1.active').data('num');
+                        prev = num !== 0 ? $('.materials1.active').prev() : $('.materials1:last-child');
+                        $('.materials1.active').removeClass('active');
+                        showNextMaterial(prev, materials1, '.page2');
+                    } else {
+                        var num = $('.materials2.active').data('num');
+                        prev = num !== 0 ? $('.materials2.active').prev() : $('.materials2:last-child');
+                        $('.materials2.active').removeClass('active');
+                        showNextMaterial(prev, materials2, '.page5');
+                    }
+                    $(prev).addClass('active');
+                }
+            }
+        },
+        triggerOnTouchEnd: false,
+        threshold: 20 // сработает через 20 пикселей
     });
 });
 
